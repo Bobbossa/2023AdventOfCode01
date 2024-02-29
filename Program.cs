cd27437274db3bb8b40d069bb02d6c14d0e5b4e1
Bobbossa <bobbossa@gmail.com>
2024-02-29 14:00:30 +0000

    CompareStringToDictionary() made. Still untested. Off to work now.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,25 @@ namespace _2023AdventOfCode01 {
     internal class Program {
         //static string[] regularDigits = ["0","1","2","3","4","5","6","7","8","9"];
         static char[] regularDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        static Dictionary<string, string> digitDic = new Dictionary<string, string>();
+        //static string[] digitDic = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
         static List<int> processedInts = new List<int>();
         static int finalTotal = 0;
 
         static void Main() {
+            //Build dictionary.
+            digitDic.Add("0","zero");
+            digitDic.Add("1", "one");
+            digitDic.Add("2", "two");
+            digitDic.Add("3", "three");
+            digitDic.Add("4", "four");
+            digitDic.Add("5", "five");
+            digitDic.Add("6", "six");
+            digitDic.Add("7", "seven");
+            digitDic.Add("8", "eight");
+            digitDic.Add("9", "nine");
+
+
             Console.ForegroundColor = ConsoleColor.Green;
             string stringToProcess = "processing file...";
             using (var sr = new StreamReader("2023AOC01Input.txt"))
@@ -29,7 +44,7 @@ namespace _2023AdventOfCode01 {
             }
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Total = " + finalTotal);
-            ReadPartialString("pleasework", 10, 4);
+            ReadPartialString("pleasework", 1, 4);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("...processing complete.");
         }
@@ -88,6 +103,19 @@ namespace _2023AdventOfCode01 {
                 Console.WriteLine(partialString);
             }
             return partialString;
+        }
+
+        //untested
+        static string CompareStringToDictionary(string input, Dictionary<string, string> dic) {
+            foreach (KeyValuePair<string,string> kvp in dic) {
+                if (input.Contains(kvp.Key)) {
+                    return kvp.Key;
+                }
+                if (input.Contains(kvp.Value)) {
+                    return kvp.Value;
+                }
+            }
+            return null;
         }
     }
 }
